cb2a99aa320a0a7bcda3f4adad12b79ef136550d
Blaže Banjanski <bakiforhire@gmail.com>
2024-09-13 19:46:11 +0200

    comments on echo.js

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,14 +1,14 @@
 import Echo from 'laravel-echo';
 
-import Pusher from 'pusher-js';
+import Pusher from 'pusher-js'; // If you're still using Pusher libraries as transport
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    wsHost: import.meta.env.VITE_REVERB_HOST, // Should resolve to localhost or the server's IP
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 6002, // Fallback to 6002 if undefined
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443, // For secure WebSocket, but not required since using http/ws
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https', // Ensure `http` is used
+    enabledTransports: ['ws'], // We only need ws for non-secure WebSocket
 });
